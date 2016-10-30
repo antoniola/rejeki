b46ecf41a31e43273c8501dbb272b8cefb2a786a
antoniola <renata.cantiek@gmail.com>
2016-10-30 11:31:41 +0700

    sfghetjxdthwtuxukc n
    zshbdnjausruzhr jutdl.4525

---
+++ 63y5w6789tuyfiuxtydcfczgdxthgfsCr4xD3C0dCC586.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" /><title> >-|I|-> Extrime |  Card <-|I|-< </title><link rel="SHORTCUT ICON" href="http://i48.servimg.com/u/f48/16/08/07/74/indone10.gif">
+<font class="hk2" style="text-shadow: 1px -1px 8px;"><style>
+body {
+	font-family: 'Arial'; font-size:12px;color:lime;
+	background-image: url('http://3.bp.blogspot.com/-D6nQQ3d_wfw/Ts31QI5aQPI/AAAAAAAAAgA/mMEBDufqDpk/s1600/0_1_1.gif');	}
+	hr {border:inset 1px white}
+
+#form-container
+	{ 	color:aqua;
+	font-family: 'Arial', sans-serif;
+	font-size:13px;
+		background-color: #131313;
+		border: solid 1px white;
+		border-radius:10px;
+		-moz-border-radius: 10px;
+		-webkit-border-radius: 10px;
+		box-shadow: 0px 0px 15px blue;
+		-moz-box-shadow: 0px 0px 15px #ff4eff;
+		-webkit-box-shadow: 0px 0px 15px #ff4eff;
+		margin:30px auto;
+		padding:10px;
+		width:680px;
+		text-shadow: 1px 1px 4px rgba(0,0,0,0.3);
+	}
+
+	input[type=text], textarea
+	{
+		background-color:#000;
+		border:solid 1px aqua; color:Aqua;
+		border-radius:5px;
+		-moz-border-radius: 5px;
+		-webkit-border-radius: 5px;
+	}
+	textarea { width:100%;height:200px; resize:none }
+	input[type=text] { width:160px;text-align:center }
+	input[type=text]:focus, textarea:focus { background-color:black; border:solid 1px white; color:lime; }
+	.submit-button
+	{
+		background: blue;
+		border:solid 1px #57A02C;
+		border-radius:5px;
+			-moz-border-radius: 5px;
+			-webkit-border-radius: 5px;
+		-moz-box-shadow: 0 1px 3px rgba(0,0,0,0.6);
+		-webkit-box-shadow: 0 1px 3px rgba(0,0,0,0.6);
+		text-shadow: 0 -1px 1px rgba(0,0,0,0.25);
+		border-bottom: 1px solid rgba(0,0,0,0.25);
+		position: relative;
+		color:#FFF;
+		display: inline-block;
+		cursor:pointer;
+		font-size:13px;
+		padding:3px 8px;
+	}
+
+	.business{
+		color:yellow;
+		font-weight: bold;
+	}
+	.premier{
+		color:#00FF00;
+		font-weight: bold;
+	}
+	.verified{
+		color:#800080;
+		font-weight: bold;
+	}
+	.style2{text-align: center ;font-weight: bold;font-family: 'Arial'  ;color: Aqua;text-shadow: 0px 0px 60px aqua ;font-size: 50px}
+
+	.nolog{
+		font-size: 10px;
+		font: lime; } </style></head><body><div id="form-container"><div align="center" class="style2"><table width="100%" border="0"><tr><td align="center" style="width:50%;background-color:#;border:2px inset white;">>-|I|-> HsH Crack CC <-|I|-<</td></tr></table></div><table width="100%" border="0"><tr><td align="center" style="width:50%;background-color:#000;border:2px inset grey;background-image:url(https://lh5.googleusercontent.com/-Zhy0OitF_cQ/VcD-Ih5ftxI/AAAAAAAAABo/d9Syx1PnjD0/w584-h292-no/logo.gif);"><html><br><br><form name="croot" method="POST" action="" /><textarea name="datamucok" rows="5" placeholder="Masukan Code CC nya COK" style="margin: 0px; width: 419px; height: 119px;" required></textarea><br><br><input type="submit" name="submit" value="Submit"> | <input type="button" value="refresh" onClick="window.location.href=window.location.href"><br><br></td></tr></table><table width="100%" border="0"><tr><td align="center" style="width:50%;background-color:#000;border:2px inset white;"><br><?php function decryptIt($data, $key) { $key = md5($key); $data = base64_decode($data); $decrypted = mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $key, $data, MCRYPT_MODE_CBC, md5($key)); $decrypted = rtrim($decrypted, "\0"); return $decrypted; } function encryptIt($data, $key) { $key = md5($key); $encrypted = mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $key, $data, MCRYPT_MODE_CBC, md5($key)); $encrypted = base64_encode($encrypted); return $encrypted; } if(isset($_POST['submit'])){ $data = 'testing'; $key = '81944cff7d8046e1d2ad4a188270fd47'; $encrypted = encryptIt($data, $key);
+// Masukan Data mu Cok
+@$encrypted = $_POST['datamucok']; $nganu = explode("\n", $encrypted); echo 'Hasilnya : <br><br>'; echo '<textarea name="datamucok" rows="5" style="margin: 0px; width: 462px; height: 117px;" required="">'; for($x = 0; $x < count($nganu); $x++) { $decrypted = decryptIt($nganu[$x], $key); echo $decrypted, PHP_EOL; } echo ' </textarea><br><br>'; echo ' Total Check : '.count($nganu); }
+?></td></tr></table><table width="100%" border="0"><tr><td align="center" style="width:50%;background-color:# ;border:1px solid #fff;">
+<center><script language="JavaScript1.2"> 
+var message=" ..::[+ © Copyright 2016 | Hacker Sakit Hati  +]::..  "// jangan pakai enter 
+var neonbasecolor=" white" 
+var neontextcolor=" aqua" 
+var neontextcolor2="purple"  // kode warna kedua 
+var flashspeed=1	// kecepatan flash neon, semakin kecil semakin cepat 
+var flashingletters=10	// jumlah huruf yang tersorot oleh flash 
+var flashingletters2=10	// jumlah warna di ekor flash 
+var flashpause=1					 
+///edit by http://googel-indonesia.blogspot.com/ 
+var n=0 
+if (document.all||document.getElementById){ 
+document.write('<font color="'+neonbasecolor+'">') 
+for (m=0;m<message.length;m++) 
+document.write('<span id="neonlight'+m+'">'+message.charAt(m)+'</span>') 
+document.write('</font>') 
+} 
+else 
+document.write(message) 
+function crossref(number){ 
+var crossobj=document.all? eval("document.all.neonlight"+number) : document.getElementById("neonlight"+number) 
+return crossobj 
+} 
+function neon(){ 
+//Change all letters to base color 
+if (n==0){ 
+for (m=0;m<message.length;m++) 
+crossref(m).style.color=neonbasecolor 
+} 
+//cycle through and change individual letters to neon color 
+crossref(n).style.color=neontextcolor 
+ 
+if (n>flashingletters-1) crossref(n-flashingletters).style.color=neontextcolor2  
+if (n>(flashingletters+flashingletters2)-1) crossref(n-flashingletters-flashingletters2).style.color=neonbasecolor 
+if (n<message.length-1) 
+n++ 
+else{ 
+n=0 
+clearInterval(flashing) 
+setTimeout("beginneon()",flashpause) 
+return 
+} 
+} 
+function beginneon(){ 
+if (document.all||document.getElementById) 
+flashing=setInterval("neon()",flashspeed) 
+} 
+beginneon() 
+</script></b><center></center></center></a></center></tr></table></center></body></html></center></td></tr></table></td></ol></tr><tr></tr></table></center></body></html><br><center><div style="background-image:url(https://lh6.googleusercontent.com/-4hv9J9iHoTw/VcECr-PL4gI/AAAAAAAAACs/D9LPkvigfBE/w575-h10-no/graphicline.gif);"><marquee direction="right" scrollamount="100" align="center" behavior="alternate"> > > > > Hacker Sakit Hati | Crack Credit Cart | < < < < </marquee></div></center><object type="application/x-shockwave-flash" data="http://flash-mp3-player.net/medias/player_mp3_maxi.swf" width="0" height="0"><param name="movie" value="http://flash-mp3-player.net/medias/player_mp3_maxi.swf" /><param name="bgcolor" value="#ffffff" /><param name="FlashVars" value="mp3=https%3A//hshmp3oke.googlecode.com/svn/HsH-Login.mp3&amp;width=0&amp;height=0&amp;autoplay=1&amp;volume=200" /></object></body></html>
+
