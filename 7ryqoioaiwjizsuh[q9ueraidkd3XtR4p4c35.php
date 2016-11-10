9f48ce3b4091ef36214d5a81848ef122ba4f50c1
antoniola <renata.cantiek@gmail.com>
2016-11-10 22:10:56 +0700

    43er35sf12gr6t5dr1xv3bg1xfd31g3g21v3f3dfvdf
    e35fd4ew6r51ad351f3s51erw35ers1351hg3zds1fg3g1jf1c

--- 7ryqoioaiwjizsuh[q9ueraidkd3XtR4p4c35.php
+++ 7ryqoioaiwjizsuh[q9ueraidkd3XtR4p4c35.php
@@ -4,10 +4,9 @@
 <input type="TEXT" name="exp" placeholder="01|16">
 <input type="TEXT" name="cvv" placeholder="600">
 <input type="TEXT" name="delim" value="|" size="1"><br>
-<textarea "="" width="500 " height="500" name="hasil" style="background-color: rgb(0, 0, 0); border: 2px inset grey; background-image: url(&quot;https://3.bp.blogspot.com/-MjJEz7wdn90/V0SufHGOmKI/AAAAAAAAAEs/5d3efodxsDwiADESfhACoo9ZH1pl1nESQCLcB/s1600/13282056_278448019158223_1304933576_n.png&quot;); background-position: center center; width: 486px; height: 447px;"></textarea></center>
+<textarea "="" width="500 " height="500" name="hasil" style="background-color: rgb(0, 0, 0); border: 2px inset grey; background-image: url(&quot;https://3.bp.blogspot.com/-MjJEz7wdn90/V0SufHGOmKI/AAAAAAAAAEs/5d3efodxsDwiADESfhACoo9ZH1pl1nESQCLcB/s1600/13282056_278448019158223_1304933576_n.png&quot;); background-position: center center; width: 486px; height: 447px;"></textarea>
 <br>
-<center>
-<input type="BUTTON" name="buat" onclick="findN(InpForm)" value="Extrap"></tr></table></center></tr></table></center>
+<input type="BUTTON" name="buat" onclick="findN(InpForm)" value="Extrap"></table></center></table></center>
 <div id="jumlah" style="display: none;"></div></form>
 
 
@@ -129,6 +128,6 @@ function findN(formName){
 
 			
 </script>
-</td></tr></table><table width="100%" border="0"><tr><td align="center" style="width:50%;background-color:# ;border:1px solid #fff;">
-</center></center></a></center></tr></table></center></body></html></center></td></tr></table></td></ol></tr>
+</td></table><table width="100%" border="0"><td align="center" style="width:50%;background-color:# ;border:1px solid #fff;">
+</center></center></a></center></table></center></body></html></center></td></tr></table></td></ol>
 </body></html>
